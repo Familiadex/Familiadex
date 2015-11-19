e4df68b50e472bfb164a676aa8904f5bbeea7941
Damian Trojnar <trojnar.damian@gmail.com>
2015-11-19 12:47:45 +0100

    Use Redis for chat userlist storage

--- web/channels/chat_channel.ex
+++ web/channels/chat_channel.ex
@@ -1,30 +1,42 @@
 defmodule Familiada.ChatUsers do
+  import Exredis
+
   defp add_user(userlist, username) do
     users = userlist || []
-    newUsers = [username] ++ users
-    newUsers
+    if Enum.member?(users, username) do
+      users
+    else
+      [username | users]
+    end
   end
 
-  defp remove_user(userlist, username) do
-    users = userlist || []
-    # TODO: real remove
-    users
+  defp remove_user([], username), do: []
+  defp remove_user([username| t], username), do: t
+  defp remove_user([h| t], un), do: [h | remove_user(t, un)]
+
+  def add(room_id, username) do
+    room = get_room(room_id)
+    room |> Dict.put("chat_userlist", add_user(room["chat_userlist"], username))
+    |> set_room(room_id)
   end
 
-  def new do
-    Agent.start_link(fn -> %{} end)
+  def remove(room_id, username) do
+    room = get_room(room_id)
+    room |> Dict.put("chat_userlist", remove_user(room["chat_userlist"], username))
+    |> set_room(room_id)
   end
 
-  def add(pid, room_id, username) do
-    Agent.update(pid, fn(h) -> Dict.put(h, room_id, add_user(h[room_id], username)) end)
+  def get(room_id) do
+    get_room(room_id)["chat_userlist"]
   end
 
-  def remove(pid, room_id, username) do
-    Agent.update(pid, fn(h) -> Dict.remove(h, room_id, remove_user(h[room_id], username)) end)
+  #### #### #### #### ####
+  defp get_room(room_id) do
+     room = start_link |> elem(1) |> query ["GET", room_id]
+     room != :undefined && Poison.decode!(room) || %{chat_userlist: []}
   end
-
-  def get(pid, room_id) do
-    Agent.get(pid, fn(h) -> h[room_id] || [] end)
+  defp set_room(room, room_id) do
+    start_link |> elem(1) |> query ["SET", room_id, Poison.encode!(room)]
   end
 end
 
@@ -35,38 +47,24 @@ defmodule Familiada.ChatChannel do
 
   def join(room_id, p, socket) do
     socket = assign(socket, :user, p["user"])
-    store = get_user_store
-    ChatUsers.add(store, room_id, p["user"]["name"])
+    ChatUsers.add(room_id, p["user"]["name"])
     send(self, :after_join)
-    {:ok, %{ userlist: ChatUsers.get(store, socket.topic) }, socket}
+    {:ok, %{ userlist: ChatUsers.get(socket.topic) }, socket}
   end
 
   def handle_info(:after_join, socket) do
-    store = get_user_store
-    broadcast socket, "back:userlist", %{ userlist: ChatUsers.get(store, socket.topic) }
+    broadcast socket, "back:userlist", %{ userlist: ChatUsers.get(socket.topic) }
     {:noreply, socket}
   end
 
   def leave(_reason, socket) do
-    store = get_user_store
-    ChatUsers.remove(store, socket.topic, socket.assigns.user["name"])
-    broadcast socket, "back:userlist", %{ userlist: ChatUsers.get(store, socket.topic) }
+    ChatUsers.remove(socket.topic, socket.assigns.user["name"])
+    broadcast socket, "back:userlist", %{ userlist: ChatUsers.get(socket.topic) }
     {:ok, socket}
   end
 
   def handle_in("front:msg", message, socket) do
     broadcast socket, "back:msg", %{content: message["content"], username: socket.assigns.user["name"]}
     {:noreply, socket}
-  end
-
-  defp get_user_store do
-    store_pid = Process.whereis(:kv)
-    store = if store_pid do
-      store_pid
-    else
-      {:ok, store} = Familiada.ChatUsers.new
-      Process.register(store, :kv)
-      store
-    end
   end
 end
