5d84a0a6d94c6b8da41a332e4910ab667d8c805a
Damian Trojnar <trojnar.damian@gmail.com>
2015-12-02 18:38:34 +0100

    Add some comments

--- web/channels/game_channel.ex
+++ web/channels/game_channel.ex
@@ -1,21 +1,43 @@
+# DOCS: http://www.phoenixframework.org/docs/channels
 defmodule Familiada.GameChannel do
+  # TODO: This is macro that add some properties to module changing into PhoenixChannel
   use Familiada.Web, :channel
+  # We can refer to Familiada.Gamestate with just GameState in this Familiada.GameChannel module
   alias Familiada.GameState
 
   # This room_id is available under socket.topic
-  def join(room_id, p, socket) do
+  # This is called when users uses "join" on channel
+  # In our case we connect to game channel in "web/static/js/app.js"
+  # Where we import import socket from "./socket" that contains socket connecting API
+  # ex. let game = socket.channel("games:GAME_ID", {player: currentUser});
+  # This is then routed here based on "web/channels/user_socket.ex" routing
+  # ex. channel "games:*", Familiada.GameChannel # routes games:GAME_ID here as room_id
+  def join(room_id, payload, socket) do
     # TODO: authentication
-    # Assigns user to socket - It will be recognized by this
-    socket = assign(socket, :player, p["player"])
+    # http://hexdocs.pm/phoenix/Phoenix.Socket.html#assign/3
+    # Assign user sent from frontend to given socket connection
+    socket = assign(socket, :player, payload["player"])
+    # FIXME: Adding ready to play in
     proper_player = Dict.put(player(socket), "ready", :false)
     game_state = GameState.update(socket.topic, player(socket)["id"], "player_joined", [proper_player])
     game_state = Dict.put(game_state, "user_id", player(socket)["id"])
+    # Callback to action that should happen after users join game room
     send(self, :after_join)
     {:ok, game_state, socket}
   end
+  # Dunno why we have to do this, but basically it's because of internal socket implementation
+  # First connection must be enstablished before we can broadcast, so this is just action that
+  # is called after we establish connection with join http://hexdocs.pm/phoenix/Phoenix.Channel.html#c:handle_info/2
+  def handle_info(:after_join, socket) do
+   game_state = GameState.get_room(socket.topic)
+   broadcast socket, "back:modelUpdate", %{ model: game_state }
+   {:noreply, socket}
+  end
 
+  # We intercept "back:modelUpdate" action that's broadcasted to all open sockets
+  # This way we can customize this action with handle_out
+  # http://hexdocs.pm/phoenix/Phoenix.Channel.html#intercept/1
   intercept ["back:modelUpdate"]
-
   def handle_out("back:modelUpdate", msg, socket) do
     model = Dict.get(msg, :model)
     IO.puts "HANDLE_OUT ***** #{Poison.encode! model}"
@@ -25,18 +47,15 @@ defmodule Familiada.GameChannel do
     {:noreply, socket}
   end
 
-  def handle_info(:after_join, socket) do
-   game_state = GameState.get_room(socket.topic)
-   broadcast socket, "back:modelUpdate", %{ model: game_state }
-   {:noreply, socket}
- end
-
+  # TO CONFIRM: Action that happens when users leaves socket ?
   def leave(_reason, socket) do
     game_state = GameState.update(socket.topic, player(socket)["id"], "player_left")
     broadcast socket, "back:modelUpdate", %{ model: game_state }
     {:ok, socket}
   end
 
+  # There we handle actions from frontend
+  # They send us cmd = %{cmd: cmdName, params[strings]}
   def handle_in("modelUpdateCmd", cmd, socket) do
     # TODO: Check if action authorized given user and state - which layer?
     game_state = GameState.update(socket.topic, player(socket)["id"], cmd["cmd"], cmd["params"])
@@ -44,12 +63,12 @@ defmodule Familiada.GameChannel do
     {:noreply, socket}
   end
 
-  #### #### #### #### ####
-
+  #### #### HELPERS #### ####
+  # Retrive player from socke
   defp player(socket) do
     socket.assigns.player
   end
-
+  # TODO: Implement this
   defp authorized?(_payload) do
     true
   end
